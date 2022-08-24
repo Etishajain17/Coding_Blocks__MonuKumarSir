5f6443aaf78fc6791c54198ab39a00d3ce04c082
EtishaJain17 <jainetisha0714>
2022-08-24 12:00:34 +0530

    Contests Codes Added

---
+++ src/Day3/Pattern1.java
@@ -0,0 +1,33 @@
+package Day3;
+/*
+      1
+     121
+    12321
+   1234321
+  123454321
+ */
+import java.util.Scanner;
+
+public class Pattern1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int space=n-1,star=1;
+        for(int i=1;i<=n;i++){
+            for (int j = 1; j <=space; j++) {
+                System.out.print("  ");
+            }
+            int val=1;
+            for(int j=1;j<=star;j++){
+                System.out.print(val+" ");
+                if(j<=star/2)
+                    val++;
+                else
+                    val--;
+            }
+            star+=2;
+            space--;
+            System.out.println();
+        }
+    }
+}
